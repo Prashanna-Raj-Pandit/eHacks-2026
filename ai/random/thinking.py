7124f96a5fa58a21a3da654f783ef350f1d52bf1
Prashanna Raj Pandit <erprp99@gmail.com>
2026-03-08 12:20:39 -0500

    ai is thinking

---
+++ ai/random/thinking.py
@@ -0,0 +1,600 @@
+# A masterpiece of computational inactivity
+# 600 lines that achieve absolutely nothing
+
+# Chapter 1: The Variables That Never Mattered
+
+x = 0
+y = 0
+z = 0
+a = 0
+b = 0
+c = 0
+nothing = None
+also_nothing = None
+still_nothing = None
+more_nothing = None
+yet_more_nothing = None
+the_void = None
+emptiness = None
+nothingness = None
+the_abyss = None
+
+# Chapter 2: Constants of No Consequence
+
+MEANINGLESS_CONSTANT = 42
+ANOTHER_MEANINGLESS_CONSTANT = 43
+YET_ANOTHER_MEANINGLESS_CONSTANT = 44
+THE_FINAL_MEANINGLESS_CONSTANT = 45
+OR_IS_IT_THE_FINAL = 46
+PROBABLY_NOT = 47
+DEFINITELY_NOT_THE_LAST = 48
+OK_THIS_IS_THE_LAST = 49
+JUST_KIDDING = 50
+TRULY_THE_LAST_ONE = 51
+
+# Chapter 3: Functions That Return Nothing
+
+def do_nothing():
+    pass
+
+def also_do_nothing():
+    pass
+
+def still_do_nothing():
+    pass
+
+def really_do_nothing():
+    pass
+
+def absolutely_do_nothing():
+    pass
+
+def genuinely_do_nothing():
+    pass
+
+def completely_do_nothing():
+    pass
+
+def utterly_do_nothing():
+    pass
+
+def entirely_do_nothing():
+    pass
+
+def wholly_do_nothing():
+    pass
+
+def totally_do_nothing():
+    pass
+
+def thoroughly_do_nothing():
+    pass
+
+def comprehensively_do_nothing():
+    pass
+
+def exhaustively_do_nothing():
+    pass
+
+def definitively_do_nothing():
+    pass
+
+def conclusively_do_nothing():
+    pass
+
+def irrevocably_do_nothing():
+    pass
+
+def undeniably_do_nothing():
+    pass
+
+def unequivocally_do_nothing():
+    pass
+
+def unambiguously_do_nothing():
+    pass
+
+# Chapter 4: Classes That Accomplish Nothing
+
+class NothingClass:
+    pass
+
+class AlsoNothingClass:
+    pass
+
+class StillNothingClass:
+    pass
+
+class EmptyClass:
+    def __init__(self):
+        pass
+
+    def do_nothing(self):
+        pass
+
+    def also_do_nothing(self):
+        pass
+
+    def still_do_nothing(self):
+        pass
+
+class AnotherEmptyClass:
+    def __init__(self):
+        self.nothing = None
+
+    def get_nothing(self):
+        return None
+
+    def set_nothing(self, value):
+        pass
+
+    def delete_nothing(self):
+        pass
+
+class YetAnotherEmptyClass:
+    def __init__(self):
+        pass
+
+    def method_one(self):
+        pass
+
+    def method_two(self):
+        pass
+
+    def method_three(self):
+        pass
+
+    def method_four(self):
+        pass
+
+    def method_five(self):
+        pass
+
+# Chapter 5: Assignments That Go Nowhere
+
+result = None
+result = None
+result = None
+result = None
+result = None
+result = None
+result = None
+result = None
+result = None
+result = None
+
+temp = 0
+temp = 0
+temp = 0
+temp = 0
+temp = 0
+temp = 0
+temp = 0
+temp = 0
+temp = 0
+temp = 0
+
+value = False
+value = False
+value = False
+value = False
+value = False
+value = False
+value = False
+value = False
+value = False
+value = False
+
+# Chapter 6: Lists Full of Nothing
+
+empty_list = []
+also_empty_list = []
+still_empty_list = []
+yet_another_empty_list = []
+one_more_empty_list = []
+another_one = []
+and_another = []
+keep_going = []
+almost_done = []
+not_really = []
+
+list_of_nones = [None, None, None, None, None]
+list_of_zeros = [0, 0, 0, 0, 0]
+list_of_falses = [False, False, False, False, False]
+list_of_empty_strings = ["", "", "", "", ""]
+list_of_empty_lists = [[], [], [], [], []]
+
+# Chapter 7: Dictionaries of Despair
+
+empty_dict = {}
+also_empty_dict = {}
+still_empty_dict = {}
+yet_another_empty_dict = {}
+one_more_empty_dict = {}
+
+dict_of_nothing = {"key": None}
+dict_of_more_nothing = {"key1": None, "key2": None}
+dict_of_even_more_nothing = {"key1": None, "key2": None, "key3": None}
+dict_of_a_lot_of_nothing = {"k1": None, "k2": None, "k3": None, "k4": None}
+dict_of_the_most_nothing = {"a": None, "b": None, "c": None, "d": None, "e": None}
+
+# Chapter 8: Loops That Loop for Nothing
+
+for i in []:
+    pass
+
+for i in []:
+    pass
+
+for i in []:
+    pass
+
+for i in []:
+    pass
+
+for i in []:
+    pass
+
+for i in range(0):
+    pass
+
+for i in range(0):
+    pass
+
+for i in range(0):
+    pass
+
+for i in range(0):
+    pass
+
+for i in range(0):
+    pass
+
+# Chapter 9: Conditions That Are Never True
+
+if False:
+    pass
+
+if False:
+    pass
+
+if False:
+    pass
+
+if False:
+    pass
+
+if False:
+    pass
+
+if None:
+    pass
+
+if None:
+    pass
+
+if None:
+    pass
+
+if None:
+    pass
+
+if None:
+    pass
+
+if 0:
+    pass
+
+if 0:
+    pass
+
+if 0:
+    pass
+
+if 0:
+    pass
+
+if 0:
+    pass
+
+# Chapter 10: Try/Excepts That Try Nothing
+
+try:
+    pass
+except Exception:
+    pass
+
+try:
+    pass
+except Exception:
+    pass
+
+try:
+    pass
+except Exception:
+    pass
+
+try:
+    pass
+except Exception:
+    pass
+
+try:
+    pass
+except Exception:
+    pass
+
+try:
+    pass
+except Exception:
+    pass
+
+try:
+    pass
+except Exception:
+    pass
+
+try:
+    pass
+except Exception:
+    pass
+
+try:
+    pass
+except Exception:
+    pass
+
+try:
+    pass
+except Exception:
+    pass
+
+# Chapter 11: Lambdas That Lambda for Nothing
+
+do_nothing_lambda = lambda: None
+also_do_nothing_lambda = lambda: None
+still_do_nothing_lambda = lambda: None
+another_useless_lambda = lambda x: None
+yet_another_useless_lambda = lambda x, y: None
+one_more_useless_lambda = lambda x, y, z: None
+the_most_useless_lambda = lambda *args: None
+truly_the_most_useless = lambda **kwargs: None
+absolutely_the_most_useless = lambda *args, **kwargs: None
+the_pinnacle_of_uselessness = lambda x=None, y=None: None
+
+# Chapter 12: Comprehensions That Comprehend Nothing
+
+nothing_comprehension = [x for x in []]
+also_nothing_comprehension = [x for x in []]
+still_nothing_comprehension = {x: x for x in []}
+yet_another_nothing_comprehension = {x for x in []}
+one_more_nothing_comprehension = (x for x in [])
+
+# Chapter 13: String Variables of No Purpose
+
+empty_string = ""
+also_empty_string = ""
+still_empty_string = ""
+whitespace = " "
+also_whitespace = "  "
+more_whitespace = "   "
+a_string_of_nothing = "nothing"
+a_string_of_something = "something"
+a_string_of_anything = "anything"
+a_string_of_everything = "everything"
+
+# Chapter 14: Arithmetic That Arrives Nowhere
+
+arithmetic_result_1 = 0 + 0
+arithmetic_result_2 = 0 - 0
+arithmetic_result_3 = 0 * 0
+arithmetic_result_4 = 1 * 1
+arithmetic_result_5 = 1 ** 1
+arithmetic_result_6 = 0 // 1
+arithmetic_result_7 = 0 % 1
+arithmetic_result_8 = (0 + 0) * (0 + 0)
+arithmetic_result_9 = (1 - 1) + (1 - 1)
+arithmetic_result_10 = ((0))
+
+# Chapter 15: Boolean Logic That Concludes Nothing
+
+bool_result_1 = True and True
+bool_result_2 = True or False
+bool_result_3 = not False
+bool_result_4 = True and True and True
+bool_result_5 = False or False or True
+bool_result_6 = not not True
+bool_result_7 = not not not False
+bool_result_8 = (True or False) and (True or False)
+bool_result_9 = True == True
+bool_result_10 = False != True
+
+# Chapter 16: More Functions, More Nothing
+
+def function_a():
+    pass
+
+def function_b():
+    pass
+
+def function_c():
+    pass
+
+def function_d():
+    pass
+
+def function_e():
+    pass
+
+def function_f():
+    pass
+
+def function_g():
+    pass
+
+def function_h():
+    pass
+
+def function_i():
+    pass
+
+def function_j():
+    pass
+
+def function_k():
+    pass
+
+def function_l():
+    pass
+
+def function_m():
+    pass
+
+def function_n():
+    pass
+
+def function_o():
+    pass
+
+def function_p():
+    pass
+
+def function_q():
+    pass
+
+def function_r():
+    pass
+
+def function_s():
+    pass
+
+def function_t():
+    pass
+
+# Chapter 17: Tuples of Triviality
+
+empty_tuple = ()
+also_empty_tuple = ()
+single_element_tuple = (None,)
+double_element_tuple = (None, None)
+triple_element_tuple = (None, None, None)
+zero_tuple = (0,)
+false_tuple = (False,)
+mixed_nothing_tuple = (None, 0, False, "", [], {})
+nested_nothing_tuple = ((None,), (None,))
+deep_nothing_tuple = (((None,),),)
+
+# Chapter 18: Sets of Insignificance
+
+empty_set = set()
+also_empty_set = set()
+still_empty_set = set()
+yet_another_empty_set = set()
+one_more_empty_set = set()
+a_set_with_one_thing = {None}
+a_set_with_two_things = {None, 0}
+a_set_with_three_things = {None, 0, False}
+
+# Chapter 19: The Grand Finale — Calling All the Nothing Functions
+
+do_nothing()
+also_do_nothing()
+still_do_nothing()
+really_do_nothing()
+absolutely_do_nothing()
+genuinely_do_nothing()
+completely_do_nothing()
+utterly_do_nothing()
+entirely_do_nothing()
+wholly_do_nothing()
+totally_do_nothing()
+thoroughly_do_nothing()
+comprehensively_do_nothing()
+exhaustively_do_nothing()
+definitively_do_nothing()
+conclusively_do_nothing()
+irrevocably_do_nothing()
+undeniably_do_nothing()
+unequivocally_do_nothing()
+unambiguously_do_nothing()
+
+function_a()
+function_b()
+function_c()
+function_d()
+function_e()
+function_f()
+function_g()
+function_h()
+function_i()
+function_j()
+function_k()
+function_l()
+function_m()
+function_n()
+function_o()
+function_p()
+function_q()
+function_r()
+function_s()
+function_t()
+
+# Epilogue: Even More Nothing, Because 600 Lines Were Requested
+
+useless_1 = None
+useless_2 = None
+useless_3 = None
+useless_4 = None
+useless_5 = None
+useless_6 = None
+useless_7 = None
+useless_8 = None
+useless_9 = None
+useless_10 = None
+useless_11 = None
+useless_12 = None
+useless_13 = None
+useless_14 = None
+useless_15 = None
+useless_16 = None
+useless_17 = None
+useless_18 = None
+useless_19 = None
+useless_20 = None
+useless_21 = None
+useless_22 = None
+useless_23 = None
+useless_24 = None
+useless_25 = None
+useless_26 = None
+useless_27 = None
+useless_28 = None
+useless_29 = None
+useless_30 = None
+useless_31 = None
+useless_32 = None
+useless_33 = None
+useless_34 = None
+useless_35 = None
+useless_36 = None
+useless_37 = None
+useless_38 = None
+useless_39 = None
+useless_40 = None
+useless_41 = None
+useless_42 = None
+useless_43 = None
+useless_44 = None
+useless_45 = None
+useless_46 = None
+useless_47 = None
+useless_48 = None
+useless_49 = None
+useless_50 = None
+
+# The Actual End.
+# Nothing was accomplished.
+# Nothing was lost.
+# Nothing was gained.
+# The universe remains indifferent.
+# Have a nice day.
